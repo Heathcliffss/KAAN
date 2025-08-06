59837353694285457512cfd4c9e765b089355364
cemehmetopatan <efsocembaba@gmail.com>
2025-08-06 12:54:00 +0300

    Menu
    Menu

--- KAAN/Assets/Scripts/Game/GameOverTest.cs
+++ KAAN/Assets/Scripts/Game/GameOverTest.cs
@@ -3,14 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GameOverTest : MonoBehaviour
 {
-    public int testScore = 1234;
+    public int testScore = 51623576;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
             string name = PlayerPrefs.GetString("CurrentPlayerName", "Ýsimsiz");
-            ScoreManager.SaveScore(name, 1234); // Örnek skor
+            ScoreManager.SaveScore(name, testScore); // Örnek skor
             SceneManager.LoadScene("MainMenu");
         }
     }
