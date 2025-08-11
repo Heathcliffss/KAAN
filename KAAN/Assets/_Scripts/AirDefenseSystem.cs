19ce394a1cdcd1d5eb540c39eab3cbc1c6261a26
cemehmetopatan <efsocembaba@gmail.com>
2025-08-11 17:35:53 +0300

    asd
    asd

--- KAAN/Assets/_Scripts/AirDefenseSystem.cs
+++ KAAN/Assets/_Scripts/AirDefenseSystem.cs
@@ -43,7 +43,7 @@ public class AirDefenseSystem : MonoBehaviour
     void ShootMissile()
     {
         GameObject missile = Instantiate(missilePrefab, firePoint.position, Quaternion.identity);
-        MissileFollow follow = missile.GetComponent<MissileFollow>();
+        MissileFollowHSS follow = missile.GetComponent<MissileFollowHSS>();
         if (follow != null)
         {
             //follow.target = target;
